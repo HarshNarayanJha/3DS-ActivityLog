42f8f9aaf59643c3841458a8a42c71f17f07ae1e
Harsh Narayan Jha <harshnj@proton.me>
2025-08-03 22:59:54 +0530

    now navigation works for all frequencies, reliably, but kinda slow

--- app/src/components/ActivityLogViewer.svelte
+++ app/src/components/ActivityLogViewer.svelte
@@ -65,38 +65,6 @@
   function onFrequencyChange(newFreq: FrequencyType) {
     const middle = zoomIntoDateRange(currentSliceDate[0], currentSliceDate[1])
     setCurrentSlice(middle)
-
-    // switch (newFreq) {
-    //   case "day": {
-    //     // zoom into the middle of the current date range
-    //     const middle = zoomIntoDateRange(currentSliceDate[0], currentSliceDate[1])
-    //     setCurrentSlice(middle)
-    //     break
-    //   }
-    //   case "week": {
-    //     const middle = zoomIntoDateRange(currentSliceDate[0], currentSliceDate[1])
-    //     setCurrentSlice(middle)
-    //     break
-    //   }
-    //   case "month": {
-    //     const middle = zoomIntoDateRange(currentSlice[0], currentSlice[1])
-    //     const monthStart = dates.at(middle)!.startOf("month")
-    //     const monthEnd = dates.at(middle)!.endOf("month")
-    //     const [rangeLow, rangeHigh] = findIndexRangeByDateRange(monthStart, monthEnd, dates)
-    //     currentSlice = [rangeLow, rangeHigh]
-    //     currentSliceDate = [dates.at(rangeLow)!, dates.at(rangeHigh - 1)!]
-    //     break
-    //   }
-    //   case "year": {
-    //     const middle = zoomIntoDateRange(currentSlice[0], currentSlice[1])
-    //     const yearStart = dates.at(middle)!.startOf("year", { useLocaleWeeks: true })
-    //     const yearEnd = dates.at(middle)!.endOf("year")
-    //     const [rangeLow, rangeHigh] = findIndexRangeByDateRange(yearStart, yearEnd, dates)
-    //     currentSlice = [rangeLow, rangeHigh]
-    //     currentSliceDate = [dates.at(rangeLow)!, dates.at(rangeHigh - 1)!]
-    //     break
-    //   }
-    // }
   }
 
   function onViewChange(newView: ListViewType) {}
@@ -174,6 +142,72 @@
         setCurrentSlice(prevDay)
         break
       }
+      case "week": {
+        // change to prev week
+        console.log("Prev slice click for week view")
+        let prevWeek
+
+        if (nonEmpty) {
+          if (currentSlice[0] > 0) {
+            prevWeek = dates.at(currentSlice[0] - 1)!
+            console.log(`Previous Non empty week is ${prevWeek.toString()}`)
+          } else {
+            console.log("Can't go back anymore")
+            break
+          }
+        } else {
+          const currentWeek = currentSliceDate[0]
+          prevWeek = currentWeek.minus({ days: 7 })
+          console.log(`Previous week is ${prevWeek.toString()}`)
+        }
+
+        setCurrentSlice(prevWeek)
+        break
+      }
+      case "month": {
+        // change to prev month
+        console.log("Prev slice click for month view")
+        let prevMonth
+
+        if (nonEmpty) {
+          if (currentSlice[0] > 0) {
+            prevMonth = dates.at(currentSlice[0] - 1)!
+            console.log(`Previous Non empty month is ${prevMonth.toString()}`)
+          } else {
+            console.log("Can't go back anymore")
+            break
+          }
+        } else {
+          const currentMonth = currentSliceDate[0]
+          prevMonth = currentMonth.minus({ month: 1 })
+          console.log(`Previous month is ${prevMonth.toString()}`)
+        }
+
+        setCurrentSlice(prevMonth)
+        break
+      }
+      case "year": {
+        // change to prev year
+        console.log("Prev slice click for year view")
+        let prevYear
+
+        if (nonEmpty) {
+          if (currentSlice[0] > 0) {
+            prevYear = dates.at(currentSlice[0] - 1)!
+            console.log(`Previous Non empty year is ${prevYear.toString()}`)
+          } else {
+            console.log("Can't go back anymore")
+            break
+          }
+        } else {
+          const currentYear = currentSliceDate[0]
+          prevYear = currentYear.minus({ year: 1 })
+          console.log(`Previous year is ${prevYear.toString()}`)
+        }
+
+        setCurrentSlice(prevYear)
+        break
+      }
     }
   }
 
@@ -200,6 +234,72 @@
         }
 
         setCurrentSlice(nextDay)
+        break
+      }
+      case "week": {
+        // change to next week
+        console.log("Next slice click for week view")
+        let nextWeek
+
+        if (nonEmpty) {
+          if (currentSlice[1] < dates.length) {
+            nextWeek = dates.at(currentSlice[1] + 1)!
+            console.log(`Next Non empty week is ${nextWeek.toString()}`)
+          } else {
+            console.log("Can't go forward anymore")
+            break
+          }
+        } else {
+          const currentWeek = currentSliceDate[1]
+          nextWeek = currentWeek.plus({ days: 7 })
+          console.log(`Next Week is ${nextWeek.toString()}`)
+        }
+
+        setCurrentSlice(nextWeek)
+        break
+      }
+      case "month": {
+        // change to next month
+        console.log("Next slice click for month view")
+        let nextMonth
+
+        if (nonEmpty) {
+          if (currentSlice[1] < dates.length) {
+            nextMonth = dates.at(currentSlice[1] + 1)!
+            console.log(`Next Non empty month is ${nextMonth.toString()}`)
+          } else {
+            console.log("Can't go forward anymore")
+            break
+          }
+        } else {
+          const currentMonth = currentSliceDate[1]
+          nextMonth = currentMonth.plus({ months: 1 })
+          console.log(`Next Month is ${nextMonth.toString()}`)
+        }
+
+        setCurrentSlice(nextMonth)
+        break
+      }
+      case "year": {
+        // change to next year
+        console.log("Next slice click for year view")
+        let nextYear
+
+        if (nonEmpty) {
+          if (currentSlice[1] < dates.length) {
+            nextYear = dates.at(currentSlice[1] + 1)!
+            console.log(`Next Non empty year is ${nextYear.toString()}`)
+          } else {
+            console.log("Can't go forward anymore")
+            break
+          }
+        } else {
+          const currentYear = currentSliceDate[1]
+          nextYear = currentYear.plus({ year: 1 })
+          console.log(`Next year is ${nextYear.toString()}`)
+        }
+
+        setCurrentSlice(nextYear)
         break
       }
     }
