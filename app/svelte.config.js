bb6f87a0f40ba17f16f54cd74b4948e29622a177
Harsh Narayan Jha <harshnj@proton.me>
2025-08-08 00:18:01 +0530

    fix sveltekit config for static site

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -6,8 +6,16 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+
   kit: {
-    adapter: adapter(), alias: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
+      precompress: false,
+      strict: true
+    }),
+    alias: {
       "$components": "./src/components",
       "$": "./src/"
     }
